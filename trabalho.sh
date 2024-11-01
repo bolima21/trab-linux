8eca7c662494d2e3e5f4e43fee25fea554eea33e
bolima21 <95056223+bolima21@users.noreply.github.com>
2024-11-01 15:31:44 -0300

    Add files via upload

--- trabalho.sh
+++ trabalho.sh
@@ -2,4 +2,33 @@
 
 # NAO APAGUE A LINHA ACIMA!
 
+#1 
+pwd
+mkdir joao
+mkdir maria
+
+#2
+cd /home/aluno/joao
+cp -r /etc/systemd /home/aluno/joao
+
+#3
+cd ..
+cd /home/aluno/maria
+cp -r /etc/passwd /home/aluno/maria
+cp -r /etc/group /home/aluno/maria
+
+#4
+cd ..
+cp -r /home/aluno/joao /home/aluno/joao_copia
+cp -r /home/aluno/maria /home/aluno/maria_copia
+
+#5
+mkdir mais_uma_pasta
+cp -r /home/aluno/maria /home/aluno/mais_uma_pasta
+cp -r /home/aluno/joao /home/aluno/mais_uma_pasta
+
+#6
+mv mais_uma_pasta/maria mais_uma_pasta/maria_aux
+mv mais_uma_pasta/joao mais_uma_pasta/maria
+mv mais_uma_pasta/maria_aux mais_uma_pasta/joao
 
